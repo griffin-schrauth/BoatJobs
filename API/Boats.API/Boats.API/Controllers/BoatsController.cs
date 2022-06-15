1958380e12e9a533465edf0299333a87f46531ce
griffin-schrauth <griffinschrauth@gmail.com>
2022-06-14 21:30:04 -0400

    add more api methods

--- API/Boats.API/Boats.API/Controllers/BoatsController.cs
+++ API/Boats.API/Boats.API/Controllers/BoatsController.cs
@@ -1,4 +1,5 @@
 ﻿using Boats.API.Data;
+using Boats.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -24,9 +25,30 @@ namespace Boats.API.Controllers
             return Ok(boats);
         }
 
-        public async Task<IActionResult> PostJob()
+        [HttpGet]
+        [Route("id:guid")]
+        [ActionName("GetJob")]
+        public async Task<IActionResult> GetJob([FromRoute] Guid id)
         {
-            
+            var card = await boatsDbContext.Boats.FirstOrDefaultAsync(x=>x.Id == id);
+            if(card != null)
+            {
+                return Ok(card);
+            }
+            return NotFound("Card not found");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddJob([FromBody] Boat boat)
+        {
+            boat.Id = Guid.NewGuid();
+            await boatsDbContext.Boats.AddAsync(boat);
+
+            await boatsDbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetJob),boat.Id,boat);
+        }
+
+        // may need to add a Delete job but want to make that specific to user database
     }
 }
