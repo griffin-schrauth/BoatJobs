d62d08d5098075b5583cadeecd11e12f06fcade3
griffin-schrauth <griffinschrauth@gmail.com>
2022-12-16 15:34:01 -0500

    Added boat configuration
    Going to laptop. Sure to change the path for smss

---
+++ API/Boats.API/Boats.API/Configurations/Entities/BoatConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Boats.API.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Boats.API.Configurations.Entities
+{
+    public class BoatConfiguration : IEntityTypeConfiguration<Boat>
+    {
+        public void Configure(EntityTypeBuilder<Boat> builder)
+        {
+            builder.HasData(
+                new Boat
+                {
+                    Name = "TestingBoat",
+                    County = "Royal Palm Beach",
+                    State = "Florida",
+                    Zipcode = "33411",
+                    JobTitle = "Cleaner",
+                    JobDesciption = "Boat is dirty",
+                    Date = "1/15/2023",
+                    Amount = 50
+                },
+                new Boat
+                {
+                    Name = "TestingBoatTwo",
+                    County = "Royal Palm Beach",
+                    State = "Florida",
+                    Zipcode = "33411",
+                    JobTitle = "Washer",
+                    JobDesciption = "Boat is Still dirty",
+                    Date = "1/16/2023",
+                    Amount = 75
+                }
+            );
+        }
+    }
+}
